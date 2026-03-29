db3a3ba97aeffa287c42160473d13f26494a8395
DavidBarker05 <davidbarker9611@gmail.com>
2026-03-29 14:00:44 +0200

    slight fix to validate

--- Assets/OurAssets/Scripts/PlatformAttach.cs
+++ Assets/OurAssets/Scripts/PlatformAttach.cs
@@ -5,11 +5,13 @@ public class PlatformAttach : MonoBehaviour
 {
     void OnValidate()
     {
+        GetComponent<Collider>().isTrigger = true; // Ensure collider is a trigger
         Debug.Assert(transform.parent, "Script needs to be attached to a game object that has a parent"); // Ensure attached to a parent
+        if (!transform.parent) return;
         Debug.Assert(transform.parent.gameObject.GetComponent<Collider>(), "Game object's parent must have a collider"); // Ensure parent has a collider
         Debug.Assert(transform.parent.parent, "Game object's parent must also have a parent"); // Ensure parent is attached to a parent
+        if (!transform.parent.parent) return;
         Debug.Assert(transform.parent.parent.lossyScale == Vector3.one, "Parent of game object's parent must have no scale applied"); // Ensure parent's parent doesn't have scale
-        GetComponent<Collider>().isTrigger = true; // Ensure collider is a trigger
     }
 
     void OnTriggerEnter(Collider other)
