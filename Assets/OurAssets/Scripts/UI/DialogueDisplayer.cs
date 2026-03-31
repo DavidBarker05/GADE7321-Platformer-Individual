1af76b426660349fd05bcb44028d26619727158d
DavidBarker05 <davidbarker9611@gmail.com>
2026-03-31 19:39:39 +0200

    fixed dialogue display stuff
    now works with italics correctly, only display the name and make it look better if there is no icon

--- Assets/OurAssets/Scripts/UI/DialogueDisplayer.cs
+++ Assets/OurAssets/Scripts/UI/DialogueDisplayer.cs
@@ -17,12 +17,23 @@ public class DialogueDisplayer : MonoBehaviour
     [SerializeField]
     Button skipAllButton;
 
+    Vector2 speakerAnchorMin;
+    Vector2 speakerAnchorMax;
+    Vector2 speakerPivot;
+    Vector2 speakerAnchoredPosition;
+    Vector2 speakerSize;
+
+    Vector2 iconSize;
+
     PlayerInputScript playerInput;
 
     DialogueItem currentItem;
 
     bool isFinishedWithCurrentItem;
+
     string currentDialogueText;
+    int currentReadIndex;
+
     float secondsPerCharacter;
     float currentReadTime;
 
@@ -37,8 +48,16 @@ public class DialogueDisplayer : MonoBehaviour
                 RetrieveCurrentDialogue();
             }
         );
-        skipButton.onClick.AddListener(() => isFinishedWithCurrentItem = true );
-        skipAllButton.onClick.AddListener(() => StopDisplayingDialogue() );
+        skipButton.onClick.AddListener(() => isFinishedWithCurrentItem = true);
+        skipAllButton.onClick.AddListener(() => StopDisplayingDialogue());
+        Debug.Log(speakerName.rectTransform.anchorMin);
+        speakerAnchorMin = speakerName.rectTransform.anchorMin;
+        speakerAnchorMax = speakerName.rectTransform.anchorMax;
+        speakerPivot = speakerName.rectTransform.pivot;
+        speakerAnchoredPosition = speakerName.rectTransform.anchoredPosition;
+        speakerSize = speakerName.rectTransform.sizeDelta;
+        iconSize = dialogueIcon.rectTransform.sizeDelta;
+        gameObject.SetActive(false);
     }
 
     void Update()
@@ -54,19 +73,67 @@ public class DialogueDisplayer : MonoBehaviour
             if (!nextButton.gameObject.activeSelf) nextButton.gameObject.SetActive(true);
             return;
         }
-        if (currentItem.charactersPerSecond < 0) dialogueText.text = currentItem.text;
+        if (currentItem.charactersPerSecond < 0) currentDialogueText = currentItem.text;
         else
         {
             currentReadTime += Time.deltaTime;
             if (currentReadTime >= secondsPerCharacter)
             {
                 int numNewCharacters = (int)(currentReadTime / secondsPerCharacter);
-                dialogueText.text = currentItem.text[..Mathf.Clamp(dialogueText.text.Length + numNewCharacters, 0, currentItem.text.Length)];
+                ReadDialogue(numNewCharacters);
+                //Debug.Log(currentItem.text[..1]);
+                //currentDialogueText = currentItem.text[..Mathf.Clamp(currentDialogueText.Length + numNewCharacters, 0, currentItem.text.Length)];
                 currentReadTime = 0f;
             }
         }
         dialogueText.text = currentDialogueText;
         isFinishedWithCurrentItem = dialogueText.text.Length == currentItem.text.Length;
+    }
+
+    string CurrentTextToDisplay(int numCharacters) => currentItem.text[..Mathf.Clamp(currentReadIndex + numCharacters, 0, currentItem.text.Length)];
+
+    void ReadDialogue(int numCharacters)
+    {
+        for (int i = 0; i < numCharacters; ++i)
+        {
+            if (currentReadIndex + 2 < currentItem.text.Length && currentItem.text.Substring(currentReadIndex, 3) == "<i>")
+            {
+                currentDialogueText = CurrentTextToDisplay(3);
+                currentReadIndex += 3;
+                i += 2;
+            }
+            else if (currentReadIndex + 3 < currentItem.text.Length && currentItem.text.Substring(currentReadIndex, 4) == "</i>")
+            {
+                currentDialogueText = CurrentTextToDisplay(4);
+                currentReadIndex += 4;
+                i += 3;
+            }
+            else
+            {
+                currentDialogueText = CurrentTextToDisplay(1);
+                ++currentReadIndex;
+            }
+        }
+    }
+
+    void DisplaySpeakerAndIcon()
+    {
+        speakerName.rectTransform.anchorMin = speakerAnchorMin;
+        speakerName.rectTransform.anchorMax = speakerAnchorMax;
+        speakerName.rectTransform.pivot = speakerPivot;
+        speakerName.rectTransform.anchoredPosition = speakerAnchoredPosition;
+        speakerName.rectTransform.sizeDelta = speakerSize;
+        dialogueIcon.gameObject.SetActive(true);
+    }
+
+    void DisplaySpeaker()
+    {
+        speakerName.rectTransform.anchorMin = new Vector2(0f, 0.5f);
+        speakerName.rectTransform.anchorMax = new Vector2(0f, 0.5f);
+        speakerName.rectTransform.pivot = new Vector2(0f, 0.5f);
+        speakerName.rectTransform.anchoredPosition = new Vector2(speakerName.rectTransform.anchoredPosition.x, 0f);
+        speakerName.rectTransform.sizeDelta = iconSize;
+        dialogueIcon.gameObject.SetActive(false);
     }
 
     void RetrieveCurrentDialogue()
@@ -80,16 +147,20 @@ public class DialogueDisplayer : MonoBehaviour
         nextButton.gameObject.SetActive(false);
         dialogueIcon.sprite = DialogueManager.Instance.CurrentDialogueIcon;
         speakerName.text = currentItem.name;
+        if (dialogueIcon.sprite) DisplaySpeakerAndIcon();
+        else DisplaySpeaker();
         dialogueText.text = "";
         dialogueText.fontSize = currentItem.fontSize;
         isFinishedWithCurrentItem = false;
         currentDialogueText = "";
+        currentReadIndex = 0;
         secondsPerCharacter = 1f / currentItem.charactersPerSecond;
         currentReadTime = 0f;
     }
 
     public void StartDisplayingDialogue(System.Action callbackFunction = null)
     {
+        playerInput ??= FindFirstObjectByType<PlayerInputScript>();
         playerInput?.DisableCharacterInput();
         gameObject.SetActive(true);
         RetrieveCurrentDialogue();
