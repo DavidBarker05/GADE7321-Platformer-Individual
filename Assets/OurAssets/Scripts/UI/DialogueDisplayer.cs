262d0d1b7b9d12f589ba28248066e9339787afc3
DavidBarker05 <davidbarker9611@gmail.com>
2026-04-30 15:14:22 +0200

    improved dialogue display
    use the built in max visible characters which means not worrying about special characters like italics and also don't need to worry about word jumping to next line because of wrap

--- Assets/OurAssets/Scripts/UI/DialogueDisplayer.cs
+++ Assets/OurAssets/Scripts/UI/DialogueDisplayer.cs
@@ -7,10 +7,10 @@ public class DialogueDisplayer : MonoBehaviour
     [SerializeField]
     Image dialogueIcon;
     [SerializeField]
-    TextMeshProUGUI speakerName;
-    [SerializeField]
-    TextMeshProUGUI dialogueText;
-    [SerializeField]
+	TMP_Text speakerName;  // David - Use TMP_Text instead of TextMeshProUGUI so that isn't only forced for UI
+	[SerializeField]
+    TMP_Text dialogueText; // David - Use TMP_Text instead of TextMeshProUGUI so that isn't only forced for UI
+	[SerializeField]
     Button nextButton;
     [SerializeField]
     Button skipButton;
@@ -31,146 +31,123 @@ public class DialogueDisplayer : MonoBehaviour
 
     bool isFinishedWithCurrentItem;
 
-    string currentDialogueText;
-    int currentReadIndex;
-
     float secondsPerCharacter;
     float currentReadTime;
 
     System.Action endCallbackFunction;
 
-    void Awake()
-    {
-        playerInput = FindFirstObjectByType<PlayerInputScript>();
-        nextButton.onClick.AddListener(
-            () => {
-                DialogueManager.Instance.LoadNextItem();
-                RetrieveCurrentDialogue();
-            }
-        );
-        skipButton.onClick.AddListener(() => isFinishedWithCurrentItem = true);
-        skipAllButton.onClick.AddListener(() => StopDisplayingDialogue());
-        speakerAnchorMin = speakerName.rectTransform.anchorMin;
-        speakerAnchorMax = speakerName.rectTransform.anchorMax;
-        speakerPivot = speakerName.rectTransform.pivot;
-        speakerAnchoredPosition = speakerName.rectTransform.anchoredPosition;
-        speakerSize = speakerName.rectTransform.sizeDelta;
-        iconSize = dialogueIcon.rectTransform.sizeDelta;
-        gameObject.SetActive(false);
-    }
+	void Awake()
+	{
+		playerInput = FindFirstObjectByType<PlayerInputScript>();
+		nextButton.onClick.AddListener(NextDialogueItem);
+		skipButton.onClick.AddListener(SkipCurrentItem);
+		skipAllButton.onClick.AddListener(StopDisplayingDialogue);
+		speakerAnchorMin = speakerName.rectTransform.anchorMin;
+		speakerAnchorMax = speakerName.rectTransform.anchorMax;
+		speakerPivot = speakerName.rectTransform.pivot;
+		speakerAnchoredPosition = speakerName.rectTransform.anchoredPosition;
+		speakerSize = speakerName.rectTransform.sizeDelta;
+		iconSize = dialogueIcon.rectTransform.sizeDelta;
+		gameObject.SetActive(false);
+	}
 
-    void Update()
-    {
-        if (currentItem == null) // Will this ever happen? I'm gonna leave it in case
-        {
-            StopDisplayingDialogue();
-            return;
-        }
-        if (isFinishedWithCurrentItem)
-        {
-            if (dialogueText.text != currentItem.text) dialogueText.text = currentItem.text;
-            if (!nextButton.gameObject.activeSelf) nextButton.gameObject.SetActive(true);
-            return;
-        }
-        if (currentItem.charactersPerSecond < 0) currentDialogueText = currentItem.text;
-        else
-        {
-            currentReadTime += Time.deltaTime;
-            if (currentReadTime >= secondsPerCharacter)
-            {
-                int numNewCharacters = (int)(currentReadTime / secondsPerCharacter);
-                ReadDialogue(numNewCharacters);
-                //Debug.Log(currentItem.text[..1]);
-                //currentDialogueText = currentItem.text[..Mathf.Clamp(currentDialogueText.Length + numNewCharacters, 0, currentItem.text.Length)];
-                currentReadTime = 0f;
-            }
-        }
-        dialogueText.text = currentDialogueText;
-        isFinishedWithCurrentItem = dialogueText.text.Length == currentItem.text.Length;
-    }
+	void OnDestroy()
+	{
+		nextButton.onClick.RemoveListener(NextDialogueItem);
+		skipButton.onClick.RemoveListener(SkipCurrentItem);
+		skipAllButton.onClick.RemoveListener(StopDisplayingDialogue);
+	}
 
-    string CurrentTextToDisplay(int numCharacters) => currentItem.text[..Mathf.Clamp(currentReadIndex + numCharacters, 0, currentItem.text.Length)];
+	void Update()
+	{
+		if (currentItem == null) // Will this ever happen? I'm gonna leave it in case
+		{
+			StopDisplayingDialogue();
+			return;
+		}
+		if (isFinishedWithCurrentItem)
+		{
+			if (dialogueText.maxVisibleCharacters != dialogueText.text.Length) dialogueText.maxVisibleCharacters = dialogueText.text.Length;
+			if (!nextButton.gameObject.activeSelf) nextButton.gameObject.SetActive(true);
+			return;
+		}
+		if (currentItem.charactersPerSecond < 0) dialogueText.maxVisibleCharacters = dialogueText.text.Length;
+		else
+		{
+			currentReadTime += Time.deltaTime;
+			if (currentReadTime >= secondsPerCharacter)
+			{
+			    int numNewCharacters = (int)(currentReadTime / secondsPerCharacter);
+				dialogueText.maxVisibleCharacters = Mathf.Clamp(dialogueText.maxVisibleCharacters + numNewCharacters, 0, dialogueText.text.Length);
+			    currentReadTime = 0f;
+			}
+		}
+		isFinishedWithCurrentItem = dialogueText.maxVisibleCharacters == dialogueText.text.Length;
+	}
 
-    void ReadDialogue(int numCharacters)
-    {
-        for (int i = 0; i < numCharacters; ++i)
-        {
-            if (currentReadIndex + 2 < currentItem.text.Length && currentItem.text.Substring(currentReadIndex, 3) == "<i>")
-            {
-                currentDialogueText = CurrentTextToDisplay(3);
-                currentReadIndex += 3;
-                i += 2;
-            }
-            else if (currentReadIndex + 3 < currentItem.text.Length && currentItem.text.Substring(currentReadIndex, 4) == "</i>")
-            {
-                currentDialogueText = CurrentTextToDisplay(4);
-                currentReadIndex += 4;
-                i += 3;
-            }
-            else
-            {
-                currentDialogueText = CurrentTextToDisplay(1);
-                ++currentReadIndex;
-            }
-        }
-    }
+	void NextDialogueItem()
+	{
+		DialogueManager.Instance.LoadNextItem();
+		RetrieveCurrentDialogue();
+	}
 
-    void DisplaySpeakerAndIcon()
-    {
-        speakerName.rectTransform.anchorMin = speakerAnchorMin;
-        speakerName.rectTransform.anchorMax = speakerAnchorMax;
-        speakerName.rectTransform.pivot = speakerPivot;
-        speakerName.rectTransform.anchoredPosition = speakerAnchoredPosition;
-        speakerName.rectTransform.sizeDelta = speakerSize;
-        dialogueIcon.gameObject.SetActive(true);
-    }
+	void SkipCurrentItem() => isFinishedWithCurrentItem = true;
 
-    void DisplaySpeaker()
-    {
-        speakerName.rectTransform.anchorMin = new Vector2(0f, 0.5f);
-        speakerName.rectTransform.anchorMax = new Vector2(0f, 0.5f);
-        speakerName.rectTransform.pivot = new Vector2(0f, 0.5f);
-        speakerName.rectTransform.anchoredPosition = new Vector2(speakerName.rectTransform.anchoredPosition.x, 0f);
-        speakerName.rectTransform.sizeDelta = iconSize;
-        dialogueIcon.gameObject.SetActive(false);
-    }
+	void DisplaySpeakerAndIcon()
+	{
+	    speakerName.rectTransform.anchorMin = speakerAnchorMin;
+	    speakerName.rectTransform.anchorMax = speakerAnchorMax;
+	    speakerName.rectTransform.pivot = speakerPivot;
+	    speakerName.rectTransform.anchoredPosition = speakerAnchoredPosition;
+	    speakerName.rectTransform.sizeDelta = speakerSize;
+	    dialogueIcon.gameObject.SetActive(true);
+	}
+	
+	void DisplaySpeaker()
+	{
+	    speakerName.rectTransform.anchorMin = new Vector2(0f, 0.5f);
+	    speakerName.rectTransform.anchorMax = new Vector2(0f, 0.5f);
+	    speakerName.rectTransform.pivot = new Vector2(0f, 0.5f);
+	    speakerName.rectTransform.anchoredPosition = new Vector2(speakerName.rectTransform.anchoredPosition.x, 0f);
+	    speakerName.rectTransform.sizeDelta = iconSize;
+	    dialogueIcon.gameObject.SetActive(false);
+	}
 
-    void RetrieveCurrentDialogue()
-    {
-        currentItem = DialogueManager.Instance.CurrentDialogueItem;
-        if (currentItem == null)
-        {
-            StopDisplayingDialogue();
-            return;
-        }
-        nextButton.gameObject.SetActive(false);
-        dialogueIcon.sprite = DialogueManager.Instance.CurrentDialogueIcon;
-        speakerName.text = currentItem.name;
-        if (dialogueIcon.sprite) DisplaySpeakerAndIcon();
-        else DisplaySpeaker();
-        dialogueText.text = "";
-        dialogueText.fontSize = currentItem.fontSize;
-        isFinishedWithCurrentItem = false;
-        currentDialogueText = "";
-        currentReadIndex = 0;
-        secondsPerCharacter = 1f / currentItem.charactersPerSecond;
-        currentReadTime = 0f;
-    }
-
-    public void StartDisplayingDialogue(System.Action callbackFunction = null)
-    {
-        playerInput ??= FindFirstObjectByType<PlayerInputScript>();
-        playerInput?.DisableCharacterInput();
-        gameObject.SetActive(true);
-        RetrieveCurrentDialogue();
-        endCallbackFunction = callbackFunction;
-    }
-
-    void StopDisplayingDialogue()
-    {
-        playerInput?.EnableCharacterInput();
-        endCallbackFunction?.Invoke();
-        endCallbackFunction = null; // Clear the callback function (is this even needed since we set it to null on start? Idk I'll leave it to be safe)
-        gameObject.SetActive(false);
-    }
+	void RetrieveCurrentDialogue()
+	{
+	    currentItem = DialogueManager.Instance.CurrentDialogueItem;
+	    if (currentItem == null)
+	    {
+	        StopDisplayingDialogue();
+	        return;
+	    }
+	    nextButton.gameObject.SetActive(false);
+	    dialogueIcon.sprite = DialogueManager.Instance.CurrentDialogueIcon;
+	    speakerName.text = currentItem.name;
+	    if (dialogueIcon.sprite) DisplaySpeakerAndIcon();
+	    else DisplaySpeaker();
+	    dialogueText.text = currentItem.text;
+	    dialogueText.fontSize = currentItem.fontSize;
+	    isFinishedWithCurrentItem = false;
+		dialogueText.maxVisibleCharacters = 0;
+	    secondsPerCharacter = 1f / currentItem.charactersPerSecond;
+	    currentReadTime = 0f;
+	}
+	
+	public void StartDisplayingDialogue(System.Action callbackFunction = null)
+	{
+	    playerInput ??= FindFirstObjectByType<PlayerInputScript>();
+	    playerInput?.DisableCharacterInput();
+	    gameObject.SetActive(true);
+	    RetrieveCurrentDialogue();
+	    endCallbackFunction = callbackFunction;
+	}
+	
+	void StopDisplayingDialogue()
+	{
+	    playerInput?.EnableCharacterInput();
+	    endCallbackFunction?.Invoke();
+	    endCallbackFunction = null; // Clear the callback function (is this even needed since we set it to null on start? Idk I'll leave it to be safe)
+	    gameObject.SetActive(false);
+	}
 }
