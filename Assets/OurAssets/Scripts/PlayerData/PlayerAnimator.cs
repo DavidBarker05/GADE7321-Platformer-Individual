49d8e2a3105a3be0276ba2f269049b61d4c19b60
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-04 20:09:39 +0200

    add smoothness between blend tree animations

--- Assets/OurAssets/Scripts/PlayerData/PlayerAnimator.cs
+++ Assets/OurAssets/Scripts/PlayerData/PlayerAnimator.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class PlayerAnimator : MonoBehaviour
 {
+	[SerializeField, Range(0f, 1f), Tooltip("Adds smoothness between animation states in blend tree")]
+	float m_DampTime = 0.1f;
+
 	public float Speed { get; set; }
 	public bool IsCrouching { get; set; }
 	public bool IsGrounded { get; set; }
@@ -15,7 +18,7 @@ public class PlayerAnimator : MonoBehaviour
 
 	void Update()
     {
-		m_Animator.SetFloat("Speed", Speed);
+		m_Animator.SetFloat("Speed", Speed, m_DampTime, Time.deltaTime);
 		m_Animator.SetBool("IsCrouching", IsCrouching);
 		m_Animator.SetBool("IsGrounded", IsGrounded);
 	}
